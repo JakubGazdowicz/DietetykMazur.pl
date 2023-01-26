bdf0c91184e6647243b96a4fdea3e1d325d7eada
Jakub Gazdowicz <jakub-gazdowicz@wp.pl>
2023-01-26 23:43:50 +0100

    Remoced span above social icons and dots under hero image

--- resources/views/components/home/hero.blade.php
+++ resources/views/components/home/hero.blade.php
@@ -21,13 +21,11 @@
                     <div class="clients pt-8">
                         <h6 class="font-normal text-xs flex items-center text-body-color dark:text-gray-300 mb-4">
                            Moje social media
-                            <span class="w-8 h-[1px] bg-body-color inline-block ml-2"></span>
                         </h6>
                         <x-social-icons></x-social-icons>
                     </div>
                 </div>
             </div>
-{{--            <div class="hidden lg:block lg:w-1/12 px-4"></div>--}}
             <div class="w-full lg:w-6/12 px-4">
                 <div class="lg:text-right lg:ml-auto">
                     <div class="relative inline-block z-10 pt-11 lg:pt-0">
@@ -36,41 +34,6 @@
                             alt="hero"
                             class="max-w-full lg:ml-auto"
                         />
-                        <span class="absolute -left-8 -bottom-2 z-[-1]">
-              <svg
-                  width="93"
-                  height="93"
-                  viewBox="0 0 93 93"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-              >
-                <circle cx="2.5" cy="2.5" r="2.5" fill="#acda29"/>
-                <circle cx="2.5" cy="24.5" r="2.5" fill="#acda29"/>
-                <circle cx="2.5" cy="46.5" r="2.5" fill="#acda29"/>
-                <circle cx="2.5" cy="68.5" r="2.5" fill="#acda29"/>
-                <circle cx="2.5" cy="90.5" r="2.5" fill="#acda29"/>
-                <circle cx="24.5" cy="2.5" r="2.5" fill="#acda29"/>
-                <circle cx="24.5" cy="24.5" r="2.5" fill="#acda29"/>
-                <circle cx="24.5" cy="46.5" r="2.5" fill="#acda29"/>
-                <circle cx="24.5" cy="68.5" r="2.5" fill="#acda29"/>
-                <circle cx="24.5" cy="90.5" r="2.5" fill="#acda29"/>
-                <circle cx="46.5" cy="2.5" r="2.5" fill="#acda29"/>
-                <circle cx="46.5" cy="24.5" r="2.5" fill="#acda29"/>
-                <circle cx="46.5" cy="46.5" r="2.5" fill="#acda29"/>
-                <circle cx="46.5" cy="68.5" r="2.5" fill="#acda29"/>
-                <circle cx="46.5" cy="90.5" r="2.5" fill="#acda29"/>
-                <circle cx="68.5" cy="2.5" r="2.5" fill="#acda29"/>
-                <circle cx="68.5" cy="24.5" r="2.5" fill="#acda29"/>
-                <circle cx="68.5" cy="46.5" r="2.5" fill="#acda29"/>
-                <circle cx="68.5" cy="68.5" r="2.5" fill="#acda29"/>
-                <circle cx="68.5" cy="90.5" r="2.5" fill="#acda29"/>
-                <circle cx="90.5" cy="2.5" r="2.5" fill="#acda29"/>
-                <circle cx="90.5" cy="24.5" r="2.5" fill="#acda29"/>
-                <circle cx="90.5" cy="46.5" r="2.5" fill="#acda29"/>
-                <circle cx="90.5" cy="68.5" r="2.5" fill="#acda29"/>
-                <circle cx="90.5" cy="90.5" r="2.5" fill="#acda29"/>
-              </svg>
-            </span>
                     </div>
                 </div>
             </div>
